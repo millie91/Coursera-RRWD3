bd0ba7012942f66dc8055a630fdfea8612f1e63a
Milica Sucevic <milicasucevic@gmail.com>
2017-03-16 19:50:56 +0000

    rspec spec/photos_spec.rb -e rq03

--- Week2/Advanced_MongoDB_Ruby_Driver/places/app/models/photo.rb
+++ Week2/Advanced_MongoDB_Ruby_Driver/places/app/models/photo.rb
@@ -13,4 +13,31 @@ def initialize(params=nil)
     @location = Point.new(params[:metadata][:location]) if !params.nil? && !params[:metadata].nil?
 end
 
+def persisted?
+	!@id.nil?
+end
+
+# getter
+def place
+	if !@place.nil?
+		Place.find(@place.to_s)
+	end
+end
+
+# setter
+def place=(p)
+	if p.is_a? String
+	 @place=BSON::ObjectId.from_string(p)
+	else 
+	 @place=p
+	end
+ end
+
+ def self.find_photos_for_place(param)
+ 	id = param.is_a?(String) ? BSON::ObjectId.from_string(param) : param
+ 	# mongo_client.database.fs.find({:metadata=>{:place=>id}})
+ 	mongo_client.database.fs.find("metadata.place": id)
+
+ end
+
 end
