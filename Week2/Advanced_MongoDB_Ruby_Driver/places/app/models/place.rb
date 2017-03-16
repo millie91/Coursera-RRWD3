99b89fe07679e4a9d2a6f4f5deaa4f8a338c5b10
Milica Sucevic <milicasucevic@gmail.com>
2017-03-16 19:22:58 +0000

    rspec spec/collection_spec.rb -e rq02

--- Week2/Advanced_MongoDB_Ruby_Driver/places/app/models/place.rb
+++ Week2/Advanced_MongoDB_Ruby_Driver/places/app/models/place.rb
@@ -1,12 +1,33 @@
 class Place
-  include ActiveModel::Model
-  attr_accessor :id, :formatted_address, :location, :address_components 
+	include ActiveModel::Model
+	attr_accessor :id, :formatted_address, :location, :address_components 
 
-  def self.mongo_client
-	Mongoid::Clients.default
-  end
+	def initialize(params)
+		@id = params[:_id].to_s
+		@formatted_address = params[:formatted_address]
+		@address_components = []
+	    if !params[:address_components].nil?
+	      address_components = params[:address_components]
+	      address_components.each { |a| @address_components << AddressComponent.new(a) }
+	    end
+		@location = Point.new(params[:geometry][:geolocation])
+	end
 
-  def self.collection
-	self.mongo_client['places']
-  end
+	def self.mongo_client
+		Mongoid::Clients.default
+	end
+
+	def persisted? 
+		!@id.nil?
+	end
+
+	def self.collection
+		self.mongo_client['places']
+	end
+
+	def self.load_all(f)
+		file=File.read(f)
+	    hash=JSON.parse(file)
+	    place=collection.insert_many(hash)
+	end
 end
