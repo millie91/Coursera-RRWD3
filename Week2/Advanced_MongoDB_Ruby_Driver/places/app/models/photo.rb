26c62924045c7696b176506812f511578eb34461
Milica Sucevic <milicasucevic@gmail.com>
2017-03-16 19:48:24 +0000

    rspec spec/photos_spec.rb -e rq02

--- Week2/Advanced_MongoDB_Ruby_Driver/places/app/models/photo.rb
+++ Week2/Advanced_MongoDB_Ruby_Driver/places/app/models/photo.rb
@@ -6,4 +6,11 @@ attr_writer :contents
 def self.mongo_client
 	Mongoid::Clients.default
 end
+
+def initialize(params=nil)
+    @id = params[:_id].to_s if !params.nil? && !params[:_id].nil?
+    @place = params[:metadata][:place] if !params.nil? && !params[:metadata][:place].nil?
+    @location = Point.new(params[:metadata][:location]) if !params.nil? && !params[:metadata].nil?
+end
+
 end
