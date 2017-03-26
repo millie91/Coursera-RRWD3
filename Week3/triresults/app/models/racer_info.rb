f5f241926e69e1b05e08ef9372bbddd68fac7847
Milica Sucevic <milicasucevic@gmail.com>
2017-03-26 09:14:44 +0100

    Racer_Info Model Created

---
+++ Week3/triresults/app/models/racer_info.rb
@@ -0,0 +1,12 @@
+class RacerInfo
+  include Mongoid::Document
+  field :_id, default: -> {racer_id}
+  field :racer_id, as: :_id
+  field :fn, type: String, as: :first_name
+  field :ln, type: String, as: :last_name
+  field :g, type: String, as: :gender
+  field :yr, type: Integer, as: :birth_year
+  field :res, type: Address, as: :residence
+
+  embedded_in :parent, polymorphic: true
+end
